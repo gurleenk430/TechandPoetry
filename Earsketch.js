f251f69a6fd20b597fbe7d705e8971993b5856c1
gurleenk430 <gurleen.k@bronxsoftware.org>
2016-05-26 04:52:25 -0700

    Update Earsketch.js

--- Earsketch.js
+++ Earsketch.js
@@ -1,1 +1,16 @@
+//		author: Gurleen
+//		description:
+//
+
+init(); 
+setTempo(120); 
+song = YG_ALT_POP_CRASH_2                        
+song2 = YG_ALT_POP_CHORDS_2
+song3 = YG_ALT_POP_GUITAR_1
+song4 = YG_ALT_POP_GUITAR_2
+fitMedia(song, 1,1,5);
+fitMedia(song2, 2,1,9);
+fitMedia(song3, 3,1,11);
+fitMedia(song3, 4,1,13.35);
+finish(); 
 
